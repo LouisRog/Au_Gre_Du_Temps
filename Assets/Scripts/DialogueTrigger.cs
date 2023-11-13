e4efe7271eb9fc8b40ca8c05778f1de096e1eaa9
yo252yo <yo252yo@gmail.com>
2023-11-13 12:44:49 +0100

    embryonary system for dialog loading

--- Assets/Scripts/DialogueTrigger.cs
+++ Assets/Scripts/DialogueTrigger.cs
@@ -1,14 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class DialogTrigger : MonoBehaviour
 {
     public Message[] messages;
     public Actor[] actors;
-    public Choice[] choices;
     public string[] stringState;
 
+    void Start()
+    {
+        string resourcesPath = "Assets/Dialogs/";
+        string[] jsonFiles = Directory.GetFiles(resourcesPath, "*.json");
+        List<Message> all_messages = new List<Message>();
+
+        foreach (string filePath in jsonFiles)
+        {
+            string json = File.ReadAllText(filePath);
+            MessageList data = JsonUtility.FromJson<MessageList>(json);
+            foreach (Message d in data.messages) {
+                all_messages.Add(d);
+            }
+        }
+        messages = all_messages.ToArray();
+        Debug.Log("Loaded this many messages: " + messages.Length);
+    }
+}
+
+public class MessageList {
+    public Message[] messages;
 }
 
 [System.Serializable]
@@ -16,9 +38,10 @@ public class Message
 {
     public int actorId;
     public int messageId;
-    public bool isChoiceNode;
     public int nextMessageId;
     public string message;
+    public ChoiceOption[] choices;
+    public Sprite backgroundImage;
 }
 
 [System.Serializable]
@@ -26,13 +49,6 @@ public class Actor
 {
     public string name;
     public Sprite sprite;
-}
-
-[System.Serializable]
-public class Choice
-{
-    public int messageNode;
-    public ChoiceOption[] choices;
 }
 
 [System.Serializable]
