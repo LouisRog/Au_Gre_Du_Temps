7632cf67aed3a25d5b3a6e246a43294b338f2778
Louis <louis.rognon@edu.gobelins.fr>
2023-11-13 15:02:29 +0100

    [ADD] TextMeshPro with UI test in Louis Scene

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -1,9 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class DialogueManager : MonoBehaviour
 {
+    public Image actorImage;
+    public TextMeshPro actorName;
+    public TextMeshPro messageText;
+    public RectTransform backgroundBox;
+
+    List<Message> currentMessages;
+    int activeMessage = 0;
+
+    public void OpenDialogue(List<Message> messages)
+    {
+        currentMessages = messages;
+        activeMessage = 0;
+    }
+
+    public void DisplayMessage()
+    {
+        Message messageToDisplay = currentMessages[activeMessage];
+        messageText.text = messageToDisplay.message;
+    }
     // Start is called before the first frame update
     void Start()
     {
