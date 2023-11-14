3cf9d0c125dafe8f1b3af0e1434176d67e483cbf
yo252yo <yo252yo@gmail.com>
2023-11-14 17:24:13 +0100

    change the control script to allow click and drag

--- Assets/Scripts/Controls.cs
+++ Assets/Scripts/Controls.cs
@@ -7,16 +7,35 @@ public class Controls : MonoBehaviour
 {
     float HorizontalMove;
     [SerializeField] public float speed = 50;
+    [SerializeField] public float mouseSpeed = 500;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {   
-        HorizontalMove = Input.GetAxis("Horizontal");
-        transform.Rotate(0,-HorizontalMove*speed*Time.deltaTime,0);
+
+    private void Update()
+    {
+        HandleKeyboardInput();
+        HandleMouseInput();
+    }
+
+    private void HandleKeyboardInput()
+    {
+        float horizontalMove = Input.GetAxis("Horizontal");
+        transform.Rotate(0, -horizontalMove * speed * Time.deltaTime, 0);
+    }
+
+    private void HandleMouseInput()
+    {
+        if (Input.GetMouseButton(0)) // Check if the left mouse button is held down
+        {
+            float mouseX = Input.GetAxis("Mouse X");
+
+            // Rotate the camera based on mouse movement
+            transform.Rotate(Vector3.up, mouseX * mouseSpeed * Time.deltaTime);
+
+        }
     }
 }
